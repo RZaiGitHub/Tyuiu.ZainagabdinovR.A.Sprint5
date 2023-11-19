0153ee10b000b1abf8d9c3dd312468f282530953
Tyuiu.ZainagabdinovR.A@outlook.com <Ruslanzai2003@gmail.com>
2023-11-19 22:10:26 +0500

    Task3

---
+++ Tyuiu.ZainagabdinovR.A.Sprint5.Task3.V12.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+
+using Tyuiu.ZainagabdinovR.A.Sprint5.Task3.V12.Lib;
+
+namespace Tyuiu.ZainagabdinovR.A.Sprint5.Task3.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string outputFile = @"C:\Users\ТИУ\source\repos\Tyuiu.ZainagabdinovR.A.Sprint5\Tyuiu.ZainagabdinovR.A.Sprint5.Task3.V12\bin\Debug\OutPutFileTask3.txt";
+
+            FileInfo fileInfo = new FileInfo(outputFile);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.ZainagabdinovR.A.Sprint5.Task3.V12/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZainagabdinovR.A.Sprint5.Task3.V12.Lib;
+
+namespace Tyuiu.ZainagabdinovR.A.Sprint5.Task3.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Зайнагабдинов Р. А. | ИСТНб-23-1";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #5.3                                                            *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Зайнагабдинов Руслан Айратович | ИСТНб-23-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить    *");
+            Console.WriteLine("* в бинарный файл OutPutFileTask3.bin и вывести на консоль.               *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("*          x^3                                                            *");
+            Console.WriteLine("*  y = -------------                                                      *");
+            Console.WriteLine("*       2(x + 5)^2                                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
